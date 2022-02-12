fce476b21cbcd8ed75fa18cb77a7d376b1bac8dd
Kaden Hansen <kadendeanhansen@gmail.com>
2022-02-12 11:03:17 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+require("dotenv").config()
+const PORT = process.env.PORT
+console.log(PORT)
+
+app.get("/", (req, res) => {
+    res.send("Bread CRUD")
+})
+
+app.listen(PORT)
