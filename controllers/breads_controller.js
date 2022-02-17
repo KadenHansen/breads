244f4273df1c5ee44d60e15a1273e1a9b1cb9cd0
Kaden Hansen <kadendeanhansen@gmail.com>
2022-02-16 21:19:06 -0500

    showing bread routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,7 +5,11 @@ const Bread = require("../models/bread.js")
 
 //Index
 breads.get("/", (req, res) => {
-    res.send(Bread)
+    res.render("index", 
+        {
+            breads: Bread
+        }
+    )
 })
 
 //Show
--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,24 @@
 const React = require('react')
 const Default = require('./layouts/Default')
+const breads = require("../models/bread")
 
 function Index () {
     return (
       <Default>
-        <h2>Index Page</h2>
+        {/* <h2>Index Page</h2>
+        <p>I have {breads[1].name} bread!</p> */}
+        <ul>
+          {
+            breads.map((bread, index) => {
+              return (
+                <li key={index}>
+                  <a href={`/bread/${index}`}>
+                    {bread.name}
+                  </a>
+                </li>)
+            })
+          }
+        </ul>
       </Default>
     )
 }
